d6b5a087f4217907f935b84c4fe7886867021116
sabrinarauj <sabrina65bina@gmail.com>
2026-03-18 16:48:11 -0300

    Aula 18/03

--- mapa/src/App.jsx
+++ mapa/src/App.jsx
@@ -1,13 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 // import './App.css'
-import Mapinha from './components/Mapinha'
+
+import QuestoesTela from './pages/QuestoesTela'
 
 function App() {
  return(
   <>
-  <Mapinha/>
+  <QuestoesTela/>
   </>
  )
 }
---
+++ mapa/src/pages/QuestoesTela.jsx
@@ -0,0 +1,12 @@
+
+export default function QuestoesTela(){
+    return (
+        <>
+        <main>
+            <header>
+                <h1>The Apothecary Diaries</h1>
+            </header>
+        </main>
+        </>
+    )
+}
